857f3274f0ab984b8818ce1b046f8c443ae8650b
Patrick Wrobel <craptik@gmail.com>
2021-06-11 02:17:13 -0400

    Added Train scene

--- scenes/Tile.gd
+++ scenes/Tile.gd
@@ -10,50 +10,64 @@ const SPRITE_SHEET_GRID_SIZE = 4
 const PIPS_TO_COLOR = {
 	0: Color("ffffff"),
 	1: Color("292929"),
-	2: Color("9b9b9b"),
+	2: Color("4dcb2f"),
 	3: Color("270437"),
 	4: Color("9b9b9b"),
 	5: Color("301dd7"),
 	6: Color("fffd6f"),
 	7: Color("8a751f"),
-	8: Color("14500d"),
-	9: Color("209bdd"),
+	8: Color("209bdd"),
+	9: Color("3f1a5f"),
 	10: Color("e7931d"),
-	11: Color("d7243d"),
-	12: Color("c62d6a"),
+	11: Color("14500d"),
+	12: Color("d7243d"),
 	13: Color("5e2f09"),
 	14: Color("e97fea"),
 	15: Color("603896"),
 }
 
 
-func _init(top_pips_ = 0, bottom_pips_ = 0):
-	self.top_pips = top_pips_
-	self.bottom_pips = bottom_pips_
-
-
 func _ready():
 	var sprite_sheet_size = $TopPips.texture.get_size()
-
 	assert(sprite_sheet_size.x == sprite_sheet_size.y)
-	var scale = sprite_sheet_size.x / SPRITE_SHEET_GRID_SIZE
 
-	$TopPips.region_rect.size = Vector2(scale, scale)
-	$TopPips.region_rect.position = Vector2(
-		scale * (self.top_pips % SPRITE_SHEET_GRID_SIZE),
-		scale * (self.top_pips / SPRITE_SHEET_GRID_SIZE)
-	)
-	$TopPips.modulate = PIPS_TO_COLOR[self.top_pips]
-
-	$BottomPips.region_rect.size = Vector2(scale, scale)
-	$BottomPips.region_rect.position = Vector2(
-		scale * (self.bottom_pips % SPRITE_SHEET_GRID_SIZE),
-		scale * (self.bottom_pips / SPRITE_SHEET_GRID_SIZE)
-	)
-	$BottomPips.modulate = PIPS_TO_COLOR[self.bottom_pips]
-
+	set_top_pips(self.top_pips)
+	set_bottom_pips(self.bottom_pips)
 	set_flipped(self.is_flipped)
 	set_face_down(self.is_face_down)
+
+
+func get_size():
+	return scale * $Surface.texture.get_size()
+
+
+func set_pips(top_pips_, bottom_pips_):
+	self.top_pips = top_pips_
+	self.bottom_pips = bottom_pips_
+	return self
+
+
+func set_top_pips(pips):
+	self.top_pips = pips
+	_set_one_pips($TopPips, self.top_pips)
+	return self
+
+
+func set_bottom_pips(pips):
+	self.bottom_pips = pips
+	_set_one_pips($BottomPips, self.bottom_pips)
+	return self
+
+
+func _set_one_pips(sprite, value):
+	var sprite_sheet_size = $TopPips.texture.get_size()
+	var scale = sprite_sheet_size.x / SPRITE_SHEET_GRID_SIZE
+
+	sprite.region_rect.size = Vector2(scale, scale)
+	sprite.region_rect.position = Vector2(
+		scale * (value % SPRITE_SHEET_GRID_SIZE), scale * (value / SPRITE_SHEET_GRID_SIZE)
+	)
+	sprite.modulate = PIPS_TO_COLOR[value]
 
 
 func set_flipped(is_flipped_):
@@ -63,6 +77,7 @@ func set_flipped(is_flipped_):
 		rotation_degrees = 180
 	else:
 		rotation_degrees = 0
+	return self
 
 
 var face_up_tex = preload("res://assets/textures/domino_front.png")
@@ -80,3 +95,5 @@ func set_face_down(is_face_down_):
 		$TopPips.visible = true
 		$BottomPips.visible = true
 		$Surface.texture = face_up_tex
+
+	return self
---
+++ scenes/Train.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+var tiles = []
+const Tile = preload("res://scenes/Tile.tscn")
+
+export var margin = Vector2(10, 10)
+export var tile_separation = 4
+
+
+func _init():
+	pass
+
+
+func _ready():
+	add_tile(Tile.instance().set_pips(1, 2))
+	add_tile(Tile.instance().set_pips(3, 4))
+	add_tile(Tile.instance().set_pips(5, 6))
+	add_tile(Tile.instance().set_pips(7, 8))
+	add_tile(Tile.instance().set_pips(9, 10))
+	add_tile(Tile.instance().set_pips(11, 12))
+	add_tile(Tile.instance().set_pips(13, 14))
+	add_tile(Tile.instance().set_pips(15, 0))
+
+
+func add_tile(tile):
+	add_child(tile)
+	tiles.append(tile)
+
+	var tile_size = tile.get_size()
+	var offset = margin + tile_size / 2
+
+	if len(tiles) % 2 == 0:
+		# Tile is on the right
+		offset.x += tile_separation + tile_size.x
+		offset.y += (tile_size.y + tile_separation) / 2
+
+	offset.y += ((len(tiles) - 1) / 2) * (tile_size.y + tile_separation)
+
+	tile.position = offset
